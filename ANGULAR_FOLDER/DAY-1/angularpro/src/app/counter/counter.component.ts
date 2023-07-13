be74054e9e9f5c5b6b2ff2cd576b3aaa730704f3
mdfaizan973 <faizan.md9735@gmail.com>
2023-07-13 13:04:41 +0530

    able to make counter

---
+++ ANGULAR_FOLDER/DAY-1/angularpro/src/app/counter/counter.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css'],
+})
+export class CounterComponent {
+  count: number = 0;
+  increment(): void {
+    this.count++;
+  }
+
+  decrement(): void {
+    this.count--;
+  }
+}
